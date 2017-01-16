d3138f99b8744f213595b40589073251def77ba7
Tiago Antao <tiagoantao@gmail.com>
2017-01-16 14:10:01 -0700

    some reproduction testing

--- tests/simulator.js
+++ tests/simulator.js
@@ -2,3 +2,22 @@ import chai from 'chai'
 var assert = chai.assert
 
 import * as simulator from '../lib/metis/simulator.js'
+import * as reproduction from '../lib/metis/operators/reproduction.js'
+import * as utils from './utils.js'
+
+describe('Basic simulation', () => {
+    it('No Ops simulation', () => {
+        const individuals = utils.generate_n_basic_individuals(10)
+        const sim_res = new simulator.cycle(individuals, [])
+        assert.equal(sim_res.individuals.length, individuals.length)
+    })
+    it('Reproduction simulation', () => {
+        let size = 10
+        let rep_size = 20
+        const individuals = utils.generate_n_basic_individuals(size)
+        let operators = [new reproduction.SexualReproduction(
+            utils.empty_species, rep_size)]
+        const sim_res = new simulator.cycle(individuals, operators)
+        assert.equal(sim_res.individuals.length, size + rep_size)
+    })
+})
--- tests/utils.js
+++ tests/utils.js
@@ -1,3 +1,10 @@
+import * as individual from '../lib/metis/individual.js'
+import * as population from '../lib/metis/population.js'
 import * as species from '../lib/metis/species.js'
 
 export let empty_species = new species.Species('empty', undefined)
+
+export let generate_n_basic_individuals = (n) => {
+    return population.generate_n_inds(n,
+        () => individual.generate_basic_individual(empty_species))
+}
