03e087d7c5973275d96d64da51221b8ce604fd74
Tiago Antao <tiagoantao@gmail.com>
2017-01-18 02:53:34 -0700

    Culling tests

--- tests/operators/culling.js
+++ tests/operators/culling.js
@@ -2,3 +2,24 @@ import chai from 'chai'
 var assert = chai.assert
 
 import * as culling from '../../lib/metis/operators/culling.js'
+import {generate_basic_individual} from '../../lib/metis/individual.js'
+import * as utils from '../utils.js'
+
+describe('Culling', () => {
+    let kill_older = new culling.KillOlderGenerations()
+    let individuals = utils.generate_n_basic_individuals(10, 0)
+    it('Remove all individuals', () => {
+        let survival = kill_older.change({}, 1, individuals, [])
+        assert.equal(survival.individuals.length, 0)
+    })
+    it('Save all individuals', () => {
+        let survival = kill_older.change({}, 0, individuals, [])
+        assert.equal(survival.individuals.length, 10)
+    })
+    it('Save one individual', () => {
+        let add_individuals = individuals.slice()
+        add_individuals.push(generate_basic_individual(utils.empty_species, 1))
+        let survival = kill_older.change({}, 1, add_individuals, [])
+        assert.equal(survival.individuals.length, 1)
+    })
+})
