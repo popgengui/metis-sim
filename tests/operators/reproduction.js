3f0361fa9702c203a32afb8b7d31b641fdaecdfb
Tiago Antao <tiagoantao@gmail.com>
2017-02-04 22:21:52 -0700

    chooser testing

--- tests/operators/reproduction.js
+++ tests/operators/reproduction.js
@@ -8,7 +8,36 @@ import {generate_basic_individual} from '../../lib/metis/integrated'
 
 import * as test_utils from '../test_utils'
 
-describe('Reproduction', () => {
+
+let male1 = generate_basic_individual(test_utils.empty_species, 0)
+male1.is_female = false
+let female1 = generate_basic_individual(test_utils.empty_species, 0)
+female1.is_female = true
+
+
+describe('Individual Choosers', () => {
+    it('Wrapper', () => {
+        let wrapper = new reproduction.WrapperChooser([male1, female1])
+        let choices = wrapper.choose()
+        let choice = choices.next()
+        assert.equal(choice.value.is_female, false)
+        choice = choices.next()
+        assert.equal(choice.value.is_female, true)
+        choice = choices.next()
+        assert.equal(choice.done, true)
+    })
+})
+
+
+describe('Mating', () => {
+})
+
+
+describe('Individual Generators', () => {
+})
+
+
+describe('Complete Reproduction', () => {
     it('Base Reproduction', () => {
         let rep = new reproduction.BaseReproduction(test_utils.empty_species, 10)
         assert.equal(rep.species, test_utils.empty_species)
@@ -23,5 +52,9 @@ describe('Reproduction', () => {
             assign_random_sex(generate_basic_individual(test_utils.empty_species)))
         let cycle = 2
         let after_change = rep.change({}, cycle, individuals, {})
+        let new_individuals = after_change.individuals
+        assert.equal(new_individuals.length, 30)
+        assert.equal(new_individuals[new_individuals.length - 1].cycle_born, 2)
+        //TBD XXX: Some more testing...
     })
 })
