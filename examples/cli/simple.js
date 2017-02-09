f08a8eebb20fbd4561b96e5442c5e7712cfdd571
Tiago Antao <tiagoantao@gmail.com>
2017-02-09 11:11:56 -0700

    simulating many cycles

--- examples/cli/simple.js
+++ examples/cli/simple.js
@@ -2,10 +2,11 @@ import {Species} from '../../lib/metis/species'
 import {assign_random_sex, generate_basic_individual} from '../../lib/metis/individual'
 import {generate_n_inds} from '../../lib/metis/population'
 
+import {KillOlderGenerations} from '../../lib/metis/operators/culling'
 import {NoGenomeSexualReproduction} from '../../lib/metis/operators/reproduction'
 import {SexStatistics} from '../../lib/metis/operators/stats/demo'
 
-import {cycle} from '../../lib/metis/simulator'
+import {cycle, do_n_cycles} from '../../lib/metis/simulator'
 
 const size = 20
 const species = new Species('test')
@@ -14,8 +15,12 @@ let individuals = generate_n_inds(size, () =>
     assign_random_sex(generate_basic_individual(species)))
 //console.log(individuals)
 
-let operators = [new NoGenomeSexualReproduction(species, size), new SexStatistics()]
+let operators = [
+    new NoGenomeSexualReproduction(species, size),
+    new KillOlderGenerations(),
+    new SexStatistics()]
 
-let state = cycle(individuals, operators)
 
+//let state = cycle(individuals, operators)
+do_n_cycles(10, individuals, operators)
 //console.log(state.individuals)
