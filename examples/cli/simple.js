fba2388bc93e46ec0376a63fdcd1cb4afb33f4d3
Tiago Antao <tiagoantao@gmail.com>
2017-02-09 10:44:51 -0700

    statistic framework - sex counts

--- examples/cli/simple.js
+++ examples/cli/simple.js
@@ -3,6 +3,7 @@ import {assign_random_sex, generate_basic_individual} from '../../lib/metis/indi
 import {generate_n_inds} from '../../lib/metis/population'
 
 import {NoGenomeSexualReproduction} from '../../lib/metis/operators/reproduction'
+import {SexStatistics} from '../../lib/metis/operators/stats/demo'
 
 import {cycle} from '../../lib/metis/simulator'
 
@@ -11,10 +12,10 @@ const species = new Species('test')
 
 let individuals = generate_n_inds(size, () =>
     assign_random_sex(generate_basic_individual(species)))
-console.log(individuals)
+//console.log(individuals)
 
-let operators = [new NoGenomeSexualReproduction(species, size)]
+let operators = [new NoGenomeSexualReproduction(species, size), new SexStatistics()]
 
 let state = cycle(individuals, operators)
 
-console.log(state.individuals)
+//console.log(state.individuals)
