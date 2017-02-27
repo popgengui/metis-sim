8bb09c0fb7371dd63ac5725e90e0679e12d5ce6f
Tiago Antao <tiagoantao@gmail.com>
2017-02-27 12:19:02 -0700

    assigning population structure

--- tests/population.js
+++ tests/population.js
@@ -13,3 +13,26 @@ describe('Population generation', () => {
         assert.equal(inds.length, 1)
     })
 })
+
+
+describe('Population structure', () => {
+    it('fixed size', () => {
+        let inds = population.generate_n_inds(2, () =>
+            individual.generate_basic_individual(utils.empty_species))
+        population.assign_fixed_size_population(inds, 2)
+        assert.equal(inds.length, 2)
+        assert.equal(inds[0].pop, 0)
+        assert.equal(inds[1].pop, 1)
+    })
+    it('random size per deme', () => {
+        let real_random = Math.random
+        Math.random = () => 0.9
+        let inds = population.generate_n_inds(2, () =>
+            individual.generate_basic_individual(utils.empty_species))
+        population.assign_random_population(inds, 2)
+        assert.equal(inds.length, 2)
+        assert.equal(inds[0].pop, 1)
+        assert.equal(inds[1].pop, 1)
+        Math.random = real_random
+    })
+})
