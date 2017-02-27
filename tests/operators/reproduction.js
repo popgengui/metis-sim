6992ce04419ba8d8b692f5b6d8c3e7eddca7b392
Tiago Antao <tiagoantao@gmail.com>
2017-02-27 16:23:56 -0700

    reproduction in structured populations

--- tests/operators/reproduction.js
+++ tests/operators/reproduction.js
@@ -3,7 +3,7 @@ var assert = chai.assert
 
 import * as reproduction from '../../lib/metis/operators/reproduction'
 import {assign_random_sex, generate_basic_individual} from '../../lib/metis/individual'
-import {generate_n_inds} from '../../lib/metis/population'
+import {assign_fixed_size_population, generate_n_inds} from '../../lib/metis/population'
 import * as integrated from '../../lib/metis/integrated'
 
 import * as test_utils from '../test_utils'
@@ -116,6 +116,28 @@ describe('Complete Reproduction', () => {
         //TBD XXX: Some more testing...
     })
     it('Sexual Reproduction - with genome', () => {
+    })
+    it('Structured Sexual Reproduction - no genome', () => {
+        let rep = new reproduction.NoGenomeStructuredSexualReproduction(
+            test_utils.empty_species, 10, 2)
+        assert.equal(rep.species, test_utils.empty_species)
+        assert.equal(rep.pop_size, 10)
+        assert.equal(rep.num_pops, 2)
+        let orig_size = 20
+        let individuals = generate_n_inds(orig_size, () =>
+            assign_random_sex(generate_basic_individual(test_utils.empty_species)))
+        assign_fixed_size_population(individuals, 2)
+        let cycle = 2
+        let state = {
+            cycle: 2,
+            individuals
+        }
+        rep.change(state)
+        assert.equal(state.individuals.length, 40)
+        assert.equal(state.individuals[state.individuals.length - 1].cycle_born, 2)
+        //TBD XXX: Some more testing...
+    })
+    it('Structured Sexual Reproduction - with genome', () => {
     })
 })
 
