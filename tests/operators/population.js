5506baeba2c01907ec6d9b9fe0ab059fc0f7039c
Tiago Antao <tiagoantao@gmail.com>
2017-03-10 14:09:59 -0700

    migration as operator

---
+++ tests/operators/population.js
@@ -0,0 +1,9 @@
+import chai from 'chai'
+var assert = chai.assert
+
+import * as reproduction from '../../lib/metis/operators/reproduction'
+
+describe('Individual Choosers', () => {
+    it('Wrapper', () => {
+    })
+})
