4c713e9cae1909bcf3d1bdfab6cf00a03973dc9e
Tiago Antao <tiagoantao@gmail.com>
2017-10-11 19:29:10 -0600

    old fashioned exports: start

--- tests/genotype.js
+++ tests/genotype.js
@@ -5,24 +5,24 @@ const genotype = require('../../lib/metis/all.js')
 
 describe('Markers without linkage information', () => {
     it('Marker', () => {
-        assert.throws(() => {new genotype.Marker()}, 'Cannot')
+        assert.throws(() => {new genotype.gn_Marker()}, 'Cannot')
     })
     it('SNP', () => {
-        let snp = new genotype.SNP()
+        let snp = new genotype.gn_SNP()
         assert.equal(snp.possible_values.length, 2)
     })
 })
 
 describe('Linkage', () => {
-    let snp1 = new genotype.SNP()
-    let snp2 = new genotype.SNP()
+    let snp1 = new genotype.gn_SNP()
+    let snp2 = new genotype.gn_SNP()
     it('Not linked', () => {
-        let unlinked = new genotype.Chromosome([snp1, snp2])
+        let unlinked = new genotype.gn_Chromosome([snp1, snp2])
         assert.equal(unlinked.markers.length, 2)
     })
     it('Linked', () => {
-        assert.throws(() => {new genotype.Chromosome([snp1, snp2], [])}, RangeError)
-        let linked = new genotype.Chromosome([snp1, snp2], [0.1])
+        assert.throws(() => {new genotype.gn_Chromosome([snp1, snp2], [])}, RangeError)
+        let linked = new genotype.gn_Chromosome([snp1, snp2], [0.1])
         assert.equal(linked.markers.length, 2)
         assert.equal(linked.distances[0], 0.1)
     })
@@ -30,11 +30,11 @@ describe('Linkage', () => {
 })
 
 describe('Autosome', () => {
-    let snp1 = new genotype.SNP()
-    let snp2 = new genotype.SNP()
-    class AutosomeSNP extends genotype.Autosome(genotype.SNP) {
+    let snp1 = new genotype.gn_SNP()
+    let snp2 = new genotype.gn_SNP()
+    class AutosomeSNP extends genotype.gn_Autosome(genotype.gn_SNP) {
     }
-    class UnlinkedAutosomeChro extends genotype.UnlinkedAutosome(genotype.Chromosome) {
+    class UnlinkedAutosomeChro extends genotype.gn_UnlinkedAutosome(genotype.gn_Chromosome) {
     }
 
     it('Single SNP', () => {
@@ -43,7 +43,7 @@ describe('Autosome', () => {
     })
     it('Two SNPs', () => {
         //needs more tests
-        let SNPs = new genotype.ChromosomePair([snp1, snp2])
+        let SNPs = new genotype.gn_ChromosomePair([snp1, snp2])
         //assert.deepEqual(SNPs.transmit([0, 1, 0, 1]), [0, 1])
     })
     it('Two SNPs - Unlinked', () => {
@@ -57,14 +57,14 @@ describe('Autosome', () => {
 
 
 describe('Genome', () => {
-    let snp11 = new genotype.SNP()
-    let snp12 = new genotype.SNP()
-    let snp21 = new genotype.SNP()
+    let snp11 = new genotype.gn_SNP()
+    let snp12 = new genotype.gn_SNP()
+    let snp21 = new genotype.gn_SNP()
     it('Two chromosomes', () => {
-        let linked = new genotype.ChromosomePair([snp11, snp12], [0.1])
-        let single = new genotype.ChromosomePair([snp21])
+        let linked = new genotype.gn_ChromosomePair([snp11, snp12], [0.1])
+        let single = new genotype.gn_ChromosomePair([snp21])
         let genome_meta = {linked, single}
-        let genome = new genotype.Genome(genome_meta)
+        let genome = new genotype.gn_Genome(genome_meta)
         let marker_order = genome.marker_order
         let first_size = genome_meta[marker_order[0]].size
         assert.equal(marker_order.length, 2)
