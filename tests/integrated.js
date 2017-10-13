fc39198af3cca7fa7e712d5e2a3015a434711809
Tiago Antao <tiagoantao@gmail.com>
2017-10-13 16:50:36 -0600

    test_utils: integrated

--- tests/integrated.js
+++ tests/integrated.js
@@ -1,8 +1,8 @@
-import chai from 'chai'
-var assert = chai.assert
+const chai = require('chai')
+const assert = chai.assert
 
-import * as integrated from '../lib/metis/integrated.js'
-import * as test_utils from './test_utils.js'
+const genotype = require('../../lib/metis/all.js')
+const test_utils = require('./test_utils.js')
 
 describe('Individual object', () => {
     it('generate_individual_with_genome', () => {
