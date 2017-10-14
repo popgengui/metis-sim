b37ea779958ca1f10e51d6fd3d42e9202fbd6f22
Tiago Antao <tiagoantao@gmail.com>
2017-10-14 10:27:57 -0600

    modules test: operators - population

--- tests/operators/population.js
+++ tests/operators/population.js
@@ -1,13 +1,12 @@
-import chai from 'chai'
-var assert = chai.assert
+const chai = require('chai')
+const assert = chai.assert
 
-import * as population from '../../lib/metis/population'
-import * as pop_ops from '../../lib/metis/operators/population'
-import * as utils from '../test_utils.js'
+const all = require('../../../lib/metis/all.js')
+const utils = require('../test_utils.js')
 
 describe('Island model migration operators', () => {
     it('fixed size population wrapper', () => {
-        let mig_op = new pop_ops.MigrationIslandFixedOperator(1)
+        let mig_op = new all.ops_p.MigrationIslandFixedOperator(1)
         let inds = utils.generate_n_basic_individuals(2)
         population.assign_fixed_size_population(inds, 2)
         population.migrate_island_fixed(inds, 1)
